51314d0ffdefa1aa0307310c1d532a94b280d0de
dera <162054865+d-1era@users.noreply.github.com>
2024-03-04 21:59:01 +0100

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,24 @@
+# Day 2: 30 Days of python programming
+first_name = 'Christine'
+last_name = 'Obidike'
+full_name = ' Christine Obidike'
+Year = '2024'
+country = 'Nigeria'
+city = 'Port-Harcourt'
+is_light_on = True
+first_name, last_name, is_light_on = 'Christine', 'Obidike', True
+
+# Checking data type
+print(type(first_name))
+print(type(True))
+print(type(2024))
+
+#using len() function
+print(len(first_name))
+
+# Aufhabe 6
+first_name = input('Enter first name: ')
+last_name = input('Enter last name: ')
+age = input('Enter age: ')
+country= input('Enter country: ')
+            
